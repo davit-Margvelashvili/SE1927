4cb7c7b9cd4dd127b6d987f0669845676a35dcb5
Davit Margvelashvili <d.margvelashvili@altasoft.ge>
2020-06-17 21:19:28 +0400

    Reflection Used For Auto Generation DTOs

---
+++ Reflection/Reflection/EmployeeDto.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+namespace ReflectionApp
+{
+	public class EmployeeDto
+	{
+		Decimal Salary { get; set; }
+		String FirstName { get; set; }
+		String LastName { get; set; }
+		Int32 Age { get; set; }
+	}
+}
---
+++ Reflection/Reflection/TeacherDto.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+
+namespace ReflectionApp
+{
+	public class TeacherDto
+	{
+		String Subject { get; set; }
+		Int32 Experience { get; set; }
+		String FirstName { get; set; }
+		String LastName { get; set; }
+		Int32 Age { get; set; }
+	}
+}
