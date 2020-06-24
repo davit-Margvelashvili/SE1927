eb73170eec100e8833a4b728039f59cf8528ee22
Davit Margvelashvili <d.margvelashvili@altasoft.ge>
2020-06-24 20:29:58 +0400

    Validate Using Data Anotations

--- XmlSerializer/XmlSerializer/Program.cs
+++ XmlSerializer/XmlSerializer/Program.cs
@@ -25,7 +25,7 @@ namespace XmlSerializer
         {
             var student = new Student
             {
-                Age = 20,
+                Age = 77,
                 FirstName = "Zura",
                 LastName = "Kusrashvili"
             };
@@ -63,16 +63,33 @@ namespace XmlSerializer
         public static IEnumerable<ValidationResult> Validate<T>(T obj)
         {
             var type = obj.GetType();
-            var properties = type
+            var propertiesWithMaxLength = type
                 .GetProperties()
                 .Where(p => p.GetCustomAttributes(typeof(MaxLengthAttribute), false).Any());
 
-            foreach (var property in properties)
+            foreach (var property in propertiesWithMaxLength)
             {
                 var attr = property.GetCustomAttribute<MaxLengthAttribute>();
 
                 if (property.GetValue(obj) is string prop && prop.Length > attr.Length)
                     yield return new ValidationResult($"Max length must be {attr.Length}", new[] { property.Name });
+            }
+
+            var propertiesWithRange = type
+                .GetProperties()
+                .Where(p => p.GetCustomAttributes<RangeAttribute>(false).Any());
+
+            foreach (var property in propertiesWithRange)
+            {
+                var attr = property.GetCustomAttribute<RangeAttribute>();
+
+                if (property.GetValue(obj) is int prop && attr.Minimum is int min && attr.Maximum is int max)
+                {
+                    if (prop < min || prop > max)
+                    {
+                        yield return new ValidationResult($"Value must be in range [{min},{max}]", new[] { property.Name });
+                    }
+                }
             }
         }
     }
